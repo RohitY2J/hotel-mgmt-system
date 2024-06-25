68b7016a83189f682df845ba61fed1e21490e21e
Samyog Adhikari <samyog.adhikari@agile.com.np>
2024-06-25 23:37:34 +0545

    tailwind angular template added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {title: 'Login', component: LoginComponent, path:'login'},
+    {title: 'Dashboard', component: DashboardComponent, path:'**'},
+    
+];
