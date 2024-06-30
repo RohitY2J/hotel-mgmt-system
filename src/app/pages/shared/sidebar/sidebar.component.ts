9d22c823ab43a58fde537eb580f663625298b0b1
Samyog Adhikari <samyog.adhikari@agile.com.np>
2024-06-30 23:11:06 +0545

    dashboard and sidebar design change

--- src/app/pages/shared/sidebar/sidebar.component.ts
+++ src/app/pages/shared/sidebar/sidebar.component.ts
@@ -2,21 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { initFlowbite  } from 'flowbite';
 import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
   constructor(private authService: AuthService) {}
+  collapseShow = "hidden";
   ngOnInit(): void {
     initFlowbite();
   }
   logout() {
     this.authService.logout();
     
+  }
+  toggleCollapseShow(classes: any) {
+    this.collapseShow = classes;
   }
 }
