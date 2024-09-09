c7853b90933cce943803ce585a306788d9c2fb7e
Rohit Kawari <rohitkauri13@gmail.com>
2024-09-09 10:42:54 +0545

    Fixed notification showing only once issue

--- src/app/pages/shared/notification/notification.component.ts
+++ src/app/pages/shared/notification/notification.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { Dismiss, DismissOptions } from 'flowbite';
 import { NotificationParameter } from '../../../models/Notification';
 
@@ -18,12 +18,16 @@ export class NotificationComponent {
     message: "Success",
     error: true
   };
+  toastVisible: boolean = false;
   dismiss: Dismiss | null = null;
   toastCustomClass: string = "";
   iconCustomClass: string = "";
   
   ngOnInit(): void {
+    const $targetEl: HTMLElement | null = document.getElementById('toast-success');
+  }
 
+  ngOnChanges(changes: SimpleChanges): void {
     if(this.notificationData.error){
       this.toastCustomClass = "dark:bg-red-500";
       this.iconCustomClass = "fa-regular fa-circle-xmark fa-lg";
@@ -32,33 +36,19 @@ export class NotificationComponent {
       this.toastCustomClass = "dark:bg-green-500";
       this.iconCustomClass="fa-regular fa-circle-check fa-lg"
     }
-
-    const $targetEl: HTMLElement | null = document.getElementById('toast-success');
-    
-    // options object
-    const options: DismissOptions = {
-      transition: 'transition-opacity',
-      duration: 0,
-      timing: 'ease-out',
-      
-      // callback functions
-      onHide: (context, targetEl) => {
-        console.log('element has been dismissed')
-      }
-    };
-    
-    if ($targetEl) {
-      this.dismiss = new Dismiss($targetEl, null, options);
-      
-      // Hide the element after 3 seconds
-      setTimeout(() => {
-        this.dismiss?.hide();
-      }, 4000);
+    if (changes['notificationData'] && changes['notificationData'].currentValue && Object.keys(changes['notificationData'].currentValue).length > 0) {
+      this.showToast();
     }
   }
 
-  dismissToast(){
-    console.log("Dismiss Toast clicked");
-    this.dismiss?.hide();
+  showToast() {
+    this.toastVisible = true;
+    setTimeout(() => {
+      this.toastVisible = false;
+    }, 5000); // Adjust timeout duration as needed
+  }
+
+  dismissToast() {
+    this.toastVisible = false;
   }
 }
