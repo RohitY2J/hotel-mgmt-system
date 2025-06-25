aa836993f4f75e12db82506fe3946fd1ace8bd45
Rohit Kawari <rohitkauri13@gmail.com>
2025-06-25 23:00:33 +0545

    added client and databaseURL

--- env/nodeEnv.js
+++ env/nodeEnv.js
@@ -1,5 +1,17 @@
 module.exports = {
+    clientNumberPair: [{
+        client: "66a51857f9bc44ed01853eb5",
+        clientName: "Atithi Satkar",
+        whatsappNumber: "9779843833224"
+    },
+    {
+        client: "669945716d8d49ed96f23fe8",
+        clientName: "Vinayak Paratha",
+        whatsappNumber: "9779843833224"
+    }],
     serverUrl: 'http://localhost:8000',
     serverPort: 8000,
-    databaseURL: 'mongodb://127.0.0.1/hotel-mgmt-app'
+    //databaseURL: 'mongodb://root:Pass123%23@37.27.195.57:27017/hotel-mgmt-app?authSource=admin',
+    //databaseURL: 'mongodb://localhost:27017/hotel-mgmt-app'
+    databaseURL: 'mongodb+srv://12100161:KTQr3ypJQpn1lL7S@cluster0.xjzz1ia.mongodb.net/hotel-mgmt-app'
 };
