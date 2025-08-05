c7a682db4e5958f6a0c01748cd1f840f5d657c8f
Rohit Kawari <rohitkauri13@gmail.com>
2025-08-05 13:42:49 +0545

    modified to allow routing to protected pages for now

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,12 +1,14 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { UserService } from './user_context.service';
 
 export const authGuard: CanActivateFn = async (route, state) => {
   const authService = inject(AuthService) as AuthService;
   const router = inject(Router) as Router;
+  const userDetail = inject(UserService) as UserService;
 
-  const userDetail: any = await authService.setUserRole();
+  //const userDetail: any = await authService.setUserRole();
 
 
   if(state.url == '/login' || state.url === '/'){
@@ -14,20 +16,20 @@ export const authGuard: CanActivateFn = async (route, state) => {
   }
   var isAuthenticated = (await authService.isAuthenticated());
   if (state.url === '/login' || state.url === '/') {
-    if (isAuthenticated && userDetail.roleID == 1) {
+    if (isAuthenticated /*&& userDetail.getUser()?.email == ""*/) {
       router.navigate(['/admin', { outlets: { main: ['dashboard'] } }]);
       return false;
     }
-    else if(isAuthenticated && userDetail && userDetail.roleID == 0){
+    else if(isAuthenticated /*&& userDetail && userDetail,g == 0*/){
         router.navigate(['/waiter'])
         return false;
     }
     return true;
   }
-  if(state.url.includes('/admin') && isAuthenticated && userDetail &&userDetail.roleID == 0){
-    router.navigate(['/waiter'])
-    return false;
-  }
+  // if(state.url.includes('/admin') && isAuthenticated /*&& userDetail &&userDetail.roleID == 0*/){
+  //   router.navigate(['/waiter'])
+  //   return false;
+  // }
   if (!isAuthenticated && !(router.url === '/login')) {
     router.navigate(['/login']);
     return false;
