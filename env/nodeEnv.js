b31e5f1d32e3838ce830a0e4cc25547e6541e407
Rohit Kawari <rohitkauri13@gmail.com>
2025-08-05 14:20:33 +0545

    changed node databaseUrl to point local

--- env/nodeEnv.js
+++ env/nodeEnv.js
@@ -12,6 +12,6 @@ module.exports = {
     serverUrl: 'http://localhost:8000',
     serverPort: 8000,
     //databaseURL: 'mongodb://root:Pass123%23@37.27.195.57:27017/hotel-mgmt-app?authSource=admin',
-    //databaseURL: 'mongodb://localhost:27017/hotel-mgmt-app'
-    databaseURL: 'mongodb://admin:yourpassword@mongodb:27017/hotelmgmt?authSource=admin'
+    databaseURL: 'mongodb://localhost:27017/hotel-mgmt-app'
+    //databaseURL: 'mongodb://admin:yourpassword@mongodb:27017/hotelmgmt?authSource=admin'
 };
