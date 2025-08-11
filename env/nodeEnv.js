b5dc3c374f7ad10cd45507eec676501172c5b3a4
Rohit Kawari <rohitkauri13@gmail.com>
2025-08-11 14:47:22 +0545

    removed unneeded env variable

--- env/nodeEnv.js
+++ env/nodeEnv.js
@@ -1,14 +1,4 @@
 module.exports = {
-    clientNumberPair: [{
-        client: "66a51857f9bc44ed01853eb5",
-        clientName: "Atithi Satkar",
-        whatsappNumber: "9779843833224"
-    },
-    {
-        client: "669945716d8d49ed96f23fe8",
-        clientName: "Vinayak Paratha",
-        whatsappNumber: "9779843833224"
-    }],
     serverUrl: 'http://localhost:8000',
     serverPort: 8000,
     //databaseURL: 'mongodb://root:Pass123%23@37.27.195.57:27017/hotel-mgmt-app?authSource=admin',
