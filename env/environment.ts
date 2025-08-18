83ed50cd05011715fb4aaf23eaa1fed1ed11498f
Rohit Kawari <rohitkauri13@gmail.com>
2025-08-18 10:24:54 +0545

    added configuration for playwright test and for docker and local

--- env/environment.ts
+++ env/environment.ts
@@ -1,9 +1,18 @@
 export const environment = {
     SERVER_URL: 'http://localhost:8000',
-    REDIRECT_URI: 'http://localhost:4200/callback',
-    CAS_SERVER_URL: 'https://localhost:7264',
-    //CAS_SERVER_URL: 'http://localhost:5000',
+    /* local */
+    //CAS_SERVER_URL: 'https://localhost:7264',
+    //REDIRECT_URI: 'http://localhost:4200/callback',
+    
+    /* docker */
+    CAS_SERVER_URL: 'http://localhost:5000',
+    REDIRECT_URI: 'http://localhost:8000/callback',
     BASE_HREF: '/',
     APPLICATION_ID: '9bd07b95-87c5-4c56-8ed3-70f50ea9a9e8',
     TENANT_ID: 'c6cea979-afdf-4756-8dd7-90af6b3edeb7'
 };
+
+export const testEnv = {
+    FRONTEND_URL: 'http://localhost:8000',
+
+}
